d7409a82dfa5ea543572bd2b3b6d1bff98859a58
zerox <zeroxleashgaming@gmail.com>
2025-02-18 19:36:45 +0700

    stp 8

--- app/src/main/java/androidx/compose/samples/crane/details/MapViewUtils.kt
+++ app/src/main/java/androidx/compose/samples/crane/details/MapViewUtils.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package androidx.compose.samples.crane.details
 
 import android.os.Bundle
@@ -21,13 +5,13 @@ import androidx.annotation.FloatRange
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
-import androidx.compose.samples.crane.R
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.MapView
+
 
 /**
  * Remembers a MapView and gives it the lifecycle of the current LifecycleOwner
@@ -54,6 +38,9 @@ fun rememberMapViewWithLifecycle(): MapView {
     return mapView
 }
 
+/**
+ * Helper function to get the lifecycle observer for MapView to ensure it follows lifecycle events.
+ */
 private fun getMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
     LifecycleEventObserver { _, event ->
         when (event) {
@@ -67,6 +54,9 @@ private fun getMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
         }
     }
 
+/**
+ * Extension function to set zoom level for the map.
+ */
 fun GoogleMap.setZoom(
     @FloatRange(from = MinZoom.toDouble(), to = MaxZoom.toDouble()) zoom: Float
 ) {
